a1f973b849f78a04c170e8f31a2d501222ccabf4
Jared Otterstatter <Jared Ottersatter>
2018-02-09 21:22:10 -0600

    Day2-Adding Drawing

--- CoinFlipSim/physics.py
+++ CoinFlipSim/physics.py
@@ -1,8 +1,12 @@
-import turtle
+import turtle as t
 def main():
-    c1 = Coin(1,2)
+    screen=t.Turtle()
+    c1 = Coin(100,200,5,5)
     print(c1)
-    return
+    c1.applyForce(Vector(2,2))
+    c1.update()
+    c1.draw(t)
+    print(c1)
 
 
 class Coin:
@@ -15,7 +19,9 @@ class Coin:
         self.vel = Vector(0,0)
         self.acc = Vector(0,0)
     def __str__(self):
-        return "Length: " + str(self.length) + " Width: "+ str(self.width)
+        outString = "Length: " + str(self.length) + " Width: " + str(self.width) +"\n"
+        outString += "Pos: "+ str(self.pos) + " Vel:" + str(self.vel) + "Acc:" +str(self.acc)
+        return outString
 
     def getLength(self):
         return self.length
@@ -26,16 +32,32 @@ class Coin:
     def getMass(self):
         return self.mass
 
-    def drawCoin(self,t):
+    def draw(self,t):
         t.pu()
-        t.goto(self.pos.getX())
+        t.goto(self.pos.getX(),self.pos.getY())
+        t.pd()
+        t.goto(self.pos.getY()+self.getWidth(),self.pos.getX())
+        t.goto(self.pos.getY()+self.getWidth(),self.pos.getX())
+        t.goto(self.pos.getY()+self.getWidth(),self.pos.getX()+self.getWidth())
+        t.goto(self.pos.getY(),self.pos.getX()+self.getWidth())
+        t.goto(self.pos.getY(),self.pos.getX())
+        t.pu()
+
+    def update(self):
+        self.vel+=self.acc
+        self.pos+=self.vel
+        self.acc=Vector(0,0)
+        return True
+
+    def applyForce(self,vec):
+        self.acc+=vec
 
 class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
     def __str__(self):
-        return "(" + self.x + ","+self.y+")"
+        return "(" + str(self.x) + ","+ str(self.y)+")"
     
     def __add__(self, other):
         return Vector(self.x + other.x,self.y + other.y)
@@ -49,6 +71,9 @@ class Vector:
 
     def getY(self):
         return self.y
+
+
+
 
 
 if __name__ == "__main__": main()
